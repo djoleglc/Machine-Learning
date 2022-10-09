21137c6ef690a2e54b81018799efe827ffa5d630
Giovanni La Cagnina <83642432+djoleglc@users.noreply.github.com>
2022-10-09 23:54:52 +0200

    Add files via upload

--- Project1/regressions.py
+++ Project1/regressions.py
@@ -27,3 +27,51 @@ def ridge_regression(y, tx, lambda_):
     w = np.linalg.solve(x_T_x, tx_t @ y)
     loss = compute_loss(y, tx, w)
     return loss, w
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+def calculate_loss_logistic(y, tx, w):
+    sig_fitted = sigmoid(tx @ w)
+    losses =  y* np.log(sig_fitted) + (1-y) * np.log(1 - sig_fitted)
+    
+    return - losses.sum()
+
+
+def calculate_gradient_logistic(y, tx, w):
+    sig_fitted = sigmoid(tx @ w)
+    return tx.transpose() @ ( sig_fitted - y[:, None] )
+
+
+def learning_by_gradient_descent_logistic(y, tx, w, gamma):
+ 
+    gradient = calculate_gradient_logistic(y, tx, w)
+    w = w-gamma*gradient 
+    loss = calculate_loss_logistic(y, tx, w)
+    return loss, w
+
+
+def logistic_regression_gradient_descent_demo(y, x, max_iter=1000, threshold=1e-8, gamma = 0.0001):
+ 
+    w = np.zeros((x.shape[1], 1))
+    losses = []
+    # start the logistic regression
+    for iter in range(max_iter):
+        # get loss and update w.
+        loss, w = learning_by_gradient_descent_logistic(y, x, w, gamma)
+        # log info
+        if iter % 100 == 0:
+            print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
+        # converge criterion
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+    return w 
+
+    
+    
+    
+    
+    
